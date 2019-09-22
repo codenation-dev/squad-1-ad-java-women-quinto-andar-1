28e5353f26c9f3cef987eda10960fb0af0c3cbc3
Bruna Iriz <bruna.iriz@opah.com.br>
2019-09-22 15:02:49 -0300

    iniciando projeto

---
+++ src/main/java/br/com/quintoandar/errorcenter/ErrorCenterApplication.java
@@ -0,0 +1,13 @@
+package br.com.quintoandar.errorcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorCenterApplication.class, args);
+	}
+
+}
