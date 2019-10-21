861c87e323dc42e3b913db1fdf134e43e835fb41
Gabriela Fabricia de Oliveira <gabriela.fabricia.de.oliveira@ibm.com>
2019-10-21 18:03:39 -0300

    correcao delete e list

--- src/main/java/br/com/quintoandar/quintolog/controller/LogController.java
+++ src/main/java/br/com/quintoandar/quintolog/controller/LogController.java
@@ -1,66 +1,62 @@
 package br.com.quintoandar.quintolog.controller;
 
-import br.com.quintoandar.quintolog.entity.Log;
-import br.com.quintoandar.quintolog.services.LogService;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.quintoandar.quintolog.entity.Log;
+import br.com.quintoandar.quintolog.services.LogService;
+
 @RestController
 @RequestMapping("/v1/logs")
 public class LogController {
 
-    @Autowired
-    private LogService logService;
+	@Autowired
+	private LogService logService;
 
-    @PostMapping(value = "/add/log")
-    public ResponseEntity<?> save(@RequestBody Log log) {
-        try {
-            logService.save(log);
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (Exception e) {
-            System.out.println("" + e);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	@PostMapping(value = "/add/log")
+	public ResponseEntity<?> save(@RequestBody Log log) {
+		try {
+			logService.save(log);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (Exception e) {
+			System.out.println("" + e);
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
-        }
-    }
+		}
+	}
 
-    @GetMapping(value = "/list/log")
-    public Object list(Pageable pageable) {
-        try {
-            return logService.listAll(pageable);
-        } catch (Exception e) {
-            System.out.println("" + e);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
+	@GetMapping(value = "/list/log")
+	public Object list(Pageable pageable) {
+		try {
+			return logService.listAll(pageable);
+		} catch (Exception e) {
+			System.out.println("" + e);
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
 
-    @GetMapping(value = "/list/log/{id}")
-    public Object listById(Long id) {
-        try {
-            return logService.listById(id);
-        } catch (Exception e) {
-            System.out.println("" + e);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
+	@GetMapping(value = "/list/log/{id}")
+	public ResponseEntity<Log> listById(@PathVariable Long id) {
+		Optional<Log> log = logService.listById(id);
+		return log.isPresent() ? ResponseEntity.ok(log.get()) : ResponseEntity.notFound().build();
+	}
 
-    @DeleteMapping(value = "/delete/log/{id}")
-    public ResponseEntity<?> delete(Long id) {
-        try {
-            logService.delete(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            System.out.println("" + e);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
+	@DeleteMapping(value = "/delete/log/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		Optional<Log> log = logService.listById(id);
+		if (!log.isPresent()) return ResponseEntity.notFound().build();
+		logService.delete(id);
+		return ResponseEntity.ok().build();
+	}
 }
