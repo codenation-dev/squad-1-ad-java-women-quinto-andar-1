aa38d60fb68cbe1cf7210850cfc71256ea4874f3
bruna-iriz <bruna.iriz@hotmail.com>
2019-11-05 09:51:43 -0300

    ajuste no script V02_0_1__create_and_register_log_user.sql

--- src/main/resources/db/migration/V02_0_1__create_and_register_log_user.sql
+++ src/main/resources/db/migration/V02_0_1__create_and_register_log_user.sql
@@ -3,8 +3,8 @@ CREATE TABLE log_user (
   name VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
-  securityQuestion BIGINT,
-  securityAnswer VARCHAR(100) NOT NULL
+  security_question BIGINT,
+  security_answer VARCHAR(100) NOT NULL
 );
 
-INSERT INTO log_user (name, email, password, securityQuestion, securityAnswer) values ('Bruna', 'bruna.iriz@gmail.com', 'test@123', '1', 'Helena')
+INSERT INTO log_user (name, email, password, security_question, security_answer) values ('Bruna', 'bruna.iriz@gmail.com', 'test@123', '1', 'Helena')
