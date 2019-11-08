8aef62c284529e7f83a1b4e2f6c7b6b539202861
clariceabreu <clacla1998@gmail.com>
2019-11-08 18:07:39 -0300

    removing permitAll for users routes

--- src/main/java/br/com/quintoandar/quintolog/config/SecurityConfigurations.java
+++ src/main/java/br/com/quintoandar/quintolog/config/SecurityConfigurations.java
@@ -40,12 +40,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
         .antMatchers(HttpMethod.POST, "/oauth/token").permitAll()
-        .antMatchers(HttpMethod.GET, "/v1/users").permitAll()
-        .antMatchers(HttpMethod.GET, "/v1/users/{id}").permitAll()
-        .antMatchers(HttpMethod.PUT, "/v1/users/changePassword/{id}").permitAll()
-        .antMatchers(HttpMethod.PUT, "/v1/users/recoverPassword").permitAll()
         .antMatchers(HttpMethod.POST, "/v1/users").permitAll()
-        .antMatchers(HttpMethod.DELETE, "/v1/users/{id}").permitAll()
 
         .anyRequest().authenticated()
         .and().csrf().disable()
