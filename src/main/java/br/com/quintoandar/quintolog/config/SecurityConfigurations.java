4d46c565241320845f37e466f2d795796d8aa0ad
Camila Araujo <mila.ag20@gmail.com>
2019-11-09 10:22:02 -0300

    authorize swagger

--- src/main/java/br/com/quintoandar/quintolog/config/SecurityConfigurations.java
+++ src/main/java/br/com/quintoandar/quintolog/config/SecurityConfigurations.java
@@ -20,6 +20,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+	
+	private static final String[] AUTH_WHITELIST = {
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };	
 
     @Autowired
     private AuthenticationService authenticationService;
@@ -38,8 +45,13 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	
+		http.cors();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/oauth/token").permitAll().antMatchers(AUTH_WHITELIST)
+				.permitAll();
+    	
         http.authorizeRequests()
-        .antMatchers(HttpMethod.POST, "/oauth/token").permitAll()
+        .antMatchers(HttpMethod.GET, "/v1/users").permitAll()
         .antMatchers(HttpMethod.POST, "/v1/users").permitAll()
         .antMatchers(HttpMethod.PUT, "/v1/recoverPassword").permitAll()
         .anyRequest().authenticated()
