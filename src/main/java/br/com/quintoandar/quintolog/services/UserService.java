7d68ecbd6f167de627c55977f2085fa34458705d
Camila Araujo <mila.ag20@gmail.com>
2019-11-09 10:49:43 -0300

    criptografar senha

--- src/main/java/br/com/quintoandar/quintolog/services/UserService.java
+++ src/main/java/br/com/quintoandar/quintolog/services/UserService.java
@@ -1,13 +1,15 @@
 package br.com.quintoandar.quintolog.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import br.com.quintoandar.quintolog.entity.LogUser;
 import br.com.quintoandar.quintolog.interfaces.InterfaceServices;
 import br.com.quintoandar.quintolog.repository.LogUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService implements InterfaceServices<Object> {
@@ -21,7 +23,11 @@ public class UserService implements InterfaceServices<Object> {
 
     @Override
     public void save(Object object) {
-        logUserRepository.save((LogUser) object);
+    	
+    	LogUser log =  (LogUser) object;
+    	log.setPassword(new BCryptPasswordEncoder().encode(log.getPassword()));
+    	
+        logUserRepository.save(log);
     }
 
     @Override
