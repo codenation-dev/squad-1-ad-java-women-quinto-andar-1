faebdeb40adba7f0315d1d9197153a823a1b1f0f
Clarice Abreu <clacla1998@gmail.com>
2019-11-12 17:29:27 -0300

    removing @EnableWebMvc

--- src/main/java/br/com/quintoandar/quintolog/config/WebConfig.java
+++ src/main/java/br/com/quintoandar/quintolog/config/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
     
     @Override
